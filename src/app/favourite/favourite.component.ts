ca7df26af1669091f029a0640da75af0c0738801
Nourhan Ashraf <nourashraf.fcih@gmail.com>
2023-06-13 17:50:28 +0300

    second

--- src/app/favourite/favourite.component.ts
+++ src/app/favourite/favourite.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-favourite',
   templateUrl: './favourite.component.html',
   styleUrls: ['./favourite.component.css'],
-  inputs: ['isFavourite']
 })
 export class FavouriteComponent {
-  isFavourite!: boolean;
+  @Input('is-favourite') isFavourite!: boolean;
 
   OnClick = () => {
     this.isFavourite = !this.isFavourite;
